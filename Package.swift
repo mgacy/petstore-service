87e068ad56caf5c029228171e77c888b18a1cdfd
Matt Gacy <matt@mgacy.com>
2023-10-14 09:51:58 -0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "petstore-service",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", .upToNextMinor(from: "0.3.0"))
+    ],
+    targets: [
+        .executableTarget(
+            name: "petstore-service",
+            dependencies: [
+                .product(
+                    name: "OpenAPIRuntime",
+                    package: "swift-openapi-runtime"
+                ),
+                .product(
+                    name: "OpenAPIURLSession",
+                    package: "swift-openapi-urlsession"
+                ),
+            ],
+            plugins: [
+                .plugin(
+                    name: "OpenAPIGenerator",
+                    package: "swift-openapi-generator"
+                )
+            ]
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Petstore Service
+
+A demo usage of [swift-openapi-generator](https://github.com/apple/swift-openapi-generator).
